58ee7b6d25230ca4f0b2b8ca8734ac240394ef0f
jsk900 <jsk900@yahoo.co.uk>
2022-03-01 11:53:08 +0100

    E08 React useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,53 @@
-import React from 'react';
+import { useState, useEffect, useRef } from 'react';
 import './App.css';
 
 const App = () => {
+  const [name, setName] = useState('');
+  const inputRef = useRef();
+  const countRef = useRef(1);
+  const previousNameRef = useRef('');
+
+  //Focus on input field
+  useEffect(() => {
+    inputRef.current.focus();
+    inputRef.current.style.backgroundColor = 'red';
+  }, []);
+
+  //Keep track of how many times our component renders
+  useEffect(() => {
+    countRef.current = countRef.current + 1;
+  }, [name]);
+
+  //Keep track of previous entry
+  useEffect(() => {
+    previousNameRef.current = name;
+  }, [name]);
+
+  console.log(countRef);
+
   return (
     <main>
-     <h1>Welcome to React</h1>
+      <input
+        type='text'
+        ref={inputRef}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <h2>{name}</h2>
+      <br />
+      <br />
+      <br />
+      <br />
+      <p>Rendered {countRef.current} times</p>
+      <br />
+      <br />
+      <br />
+      <br />
+      <p>
+        Previous name was {previousNameRef.current}, with a length of{' '}
+        {previousNameRef.current.length}
+      </p>
     </main>
   );
-}
+};
 
 export default App;
-
